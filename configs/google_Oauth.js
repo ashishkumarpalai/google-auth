c10f319a6758fb03fb277fbf60a9077bf47eaa10
Ashish kumar palai <ashishkumarpalai2000@gmail.com>
2023-12-07 19:56:42 +0530

    code update

--- configs/google_Oauth.js
+++ configs/google_Oauth.js
@@ -9,7 +9,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "http://localhost:3000/auth/google/callback" //port ye hi rakhlo
+            callbackURL: "https://google-auth-5vq1.onrender.com/auth/google/callback" //port ye hi rakhlo
         },
         async function (request, accessToken, refreshToken, profile, cb) {
             let email = profile._json.email;
