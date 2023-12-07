035b4751b0277be48831327744f5b02f164cffe7
Ashish kumar palai <ashishkumarpalai2000@gmail.com>
2023-12-07 19:12:49 +0530

    code update

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 
 const express = require('express');
 const { passport } = require("./configs/google_Oauth")
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express()
 app.use(express.json());
-
+app.use(cors())
 
 app.get("/", (req, res) => {
-    res.send("good to go")
+    res.send("google auth backend")
     console.log("Working");
 })
 
@@ -32,7 +34,11 @@ app.get(
 );
 
 
-
-app.listen(3000, () => {
-    console.log("connected to server");
-})
+app.listen(process.env.port, () => {
+    try {
+        console.log(`Server is running at port ${process.env.port}`);
+    } catch (error) {
+        console.log(error.message);
+    }
+    
+});
